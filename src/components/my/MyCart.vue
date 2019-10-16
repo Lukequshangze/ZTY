b5fcb1d9eceb8159fd66673f27d716e22730916a
hxx <hxx702@qq.com>
2019-10-16 15:59:10 +0800

    郝晓昕 徐翔 葛晓赛 添加my 文件夹

---
+++ src/components/my/MyCart.vue
@@ -0,0 +1,65 @@
+<template>
+	<div id="myCart">
+		<ul id="cartList">
+			<li id="cartItem" v-for="(item,i) of cartText" :key="i">
+				<router-link to="./MyOrder" slot="left">
+					<img :src="item.icon" alt="" id="cartIcon">
+					<p>{{item.title}}</p>
+				</router-link>
+			</li>
+		</ul>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+			cartText:[
+	          {icon:require("../../../static/myimg/mycart/order.png"),title:"全部订单"},
+	          {icon:require("../../../static/myimg/mycart/pay.png"),title:"待支付"},
+	          {icon:require("../../../static/myimg/mycart/trip.png"),title:"待出行"},
+	          {icon:require("../../../static/myimg/mycart/comment.png"),title:"待评价"}
+        		]
+			}
+		}
+	}
+</script>
+<style>
+	#myCart{
+		    margin: 0 auto;
+		    margin-top: 50px;
+		width:95%;
+		height:80px;
+		border-radius:8px;
+		background:#fff;
+		/* box-shadow:0px 0px 4px 2px rgb(235,234,234);
+		 */
+       		 box-shadow:0px 2px 5px  rgb(215,215,215);
+
+	}
+	#cartList{
+		display:flex;
+		justify-content:space-around;
+		list-style:none;
+		padding:0;
+
+	}
+	#cartItem{
+		margin:0;
+	}
+	#cartItem p{
+		color:rgb(155,155,155);
+		font-size:12px;
+		text-decoration:none;
+		display:block;margin:0;
+		padding:0
+	}
+	#cartItem img{
+		width:30px;
+		height:30px;
+		margin-top:15px;
+	}
+	#cartItem a{
+		text-decoration:none 
+	}
+</style>
---
+++ src/components/my/MyLoginHeader.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    
+    <div class="set">
+        <img src="../../../static/my_personal/set.png" alt="" width="25px" height="25px">
+    </div>
+    <div class="tou">
+        <img src="../../../static/my_personal/brand.png" alt="" width="70px" height="70px" >
+        <h4>登录/注册</h4>
+    </div>
+    <div style="height:20px"></div>
+    <div class="mili">
+        <a href="javascript:;">查看米粒</a>
+        <a href="javascript:;">查看优惠券</a>
+    </div>
+</div>
+</template>
+<script>
+    export default {
+        data(){
+            return{
+                flag:true,
+                loginLabel:"登录/注册",
+                uname:''
+            }
+        },
+        methods:{}
+    } 
+</script>
+<style scoped>
+.set{
+
+width: 95%;
+position:fixed;
+top:0;
+padding-top:15px;
+background-color: #fff;
+text-align: right;
+}
+.tou{
+    display: flex;
+    text-align: left;
+    padding-left:10px;
+    line-height:center;
+}
+.tou>h4{
+    padding-left: 20px;
+    font-size:16px;
+}
+.mili>a{
+    text-decoration: none;
+    color:rgb(67,184,236);
+    padding-left:50px;
+    padding-right:50px;
+}
+</style>
